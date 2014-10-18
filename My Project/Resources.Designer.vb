1061c20d75c455b40785f597e41027c1dd139090
jlesquivel <jlesquivel@hotmail.com>
2014-10-17 20:19:23 -0600

    17-10-2014

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -193,6 +193,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property mail() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("mail", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property money2() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("money2", resourceCulture)
